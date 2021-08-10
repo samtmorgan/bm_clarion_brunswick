d8ee05c443cd4810a278658b41f9d2909b936ec4
Samuel Morgan <74990386+samumo@users.noreply.github.com>
2021-08-10 15:22:42 +0100

    Update App.js

--- b_0.1/src/App.js
+++ b_0.1/src/App.js
@@ -108,94 +108,51 @@ class App extends React.Component {
                     {plotNumber:'C 4-7',level:'4',block:'C',number:'121',beds:'3',area:78.7}
                   ],
       filteredApartments: [ 
-                    {plotNumber:'B 0-1',level:'0',block:'B',number:'39',beds:'2',area:69.9},
-                    {plotNumber:'B 0-2',level:'0',block:'B',number:'40',beds:'2',area:65},
-                    {plotNumber:'B 0-3',level:'0',block:'B',number:'41',beds:'1',area:51.3},
-                    {plotNumber:'B 0-4',level:'0',block:'B',number:'42',beds:'2',area:76.4},
-                    {plotNumber:'B 0-5',level:'0',block:'B',number:'43',beds:'2',area:76},
-                    {plotNumber:'B 0-6',level:'0',block:'B',number:'44',beds:'1',area:53.5},
-                    {plotNumber:'B 0-7',level:'0',block:'B',number:'45',beds:'2',area:65},
-                    {plotNumber:'B 0-8',level:'0',block:'B',number:'46',beds:'2',area:69},
-                    {plotNumber:'B 0-9',level:'0',block:'B',number:'47',beds:'1',area:55.3},
-                    {plotNumber:'B 1-1',level:'1',block:'B',number:'48',beds:'1',area:68.8},
-                    {plotNumber:'B 1-2',level:'1',block:'B',number:'49',beds:'2',area:83.8},
-                    {plotNumber:'B 1-3',level:'1',block:'B',number:'50',beds:'2',area:65},
-                    {plotNumber:'B 1-4',level:'1',block:'B',number:'51',beds:'1',area:51.3},
-                    {plotNumber:'B 1-5',level:'1',block:'B',number:'52',beds:'2',area:76.4},
-                    {plotNumber:'B 1-6',level:'1',block:'B',number:'53',beds:'2',area:76},
-                    {plotNumber:'B 1-7',level:'1',block:'B',number:'54',beds:'1',area:53.5},
-                    {plotNumber:'B 1-8',level:'1',block:'B',number:'55',beds:'2',area:65},
-                    {plotNumber:'B 1-9',level:'1',block:'B',number:'56',beds:'2',area:83.8},
-                    {plotNumber:'B 1-10',level:'1',block:'B',number:'57',beds:'1',area:55.3},
-                    {plotNumber:'B 2-1',level:'2',block:'B',number:'58',beds:'1',area:68.8},
-                    {plotNumber:'B 2-2',level:'2',block:'B',number:'59',beds:'2',area:83.8},
-                    {plotNumber:'B 2-3',level:'2',block:'B',number:'60',beds:'2',area:65},
-                    {plotNumber:'B 2-4',level:'2',block:'B',number:'61',beds:'1',area:51.3},
-                    {plotNumber:'B 2-5',level:'2',block:'B',number:'62',beds:'2',area:76.4},
-                    {plotNumber:'B 2-6',level:'2',block:'B',number:'63',beds:'2',area:76},
-                    {plotNumber:'B 2-7',level:'2',block:'B',number:'64',beds:'1',area:53.5},
-                    {plotNumber:'B 2-8',level:'2',block:'B',number:'65',beds:'2',area:65},
-                    {plotNumber:'B 2-9',level:'2',block:'B',number:'66',beds:'2',area:83.8},
-                    {plotNumber:'B 2-10',level:'2',block:'B',number:'67',beds:'1',area:55.3},
-                    {plotNumber:'B 3-1',level:'3',block:'B',number:'68',beds:'2',area:76.5},
-                    {plotNumber:'B 3-2',level:'3',block:'B',number:'69',beds:'1',area:51.3},
-                    {plotNumber:'B 3-3',level:'3',block:'B',number:'70',beds:'3',area:80.6},
-                    {plotNumber:'B 3-4',level:'3',block:'B',number:'71',beds:'1',area:53.5},
-                    {plotNumber:'B 3-5',level:'3',block:'B',number:'72',beds:'2',area:76.5},
-                    {plotNumber:'B 3-6',level:'3',block:'B',number:'73',beds:'3',area:111.5},
-                    {plotNumber:'B 3-7',level:'3',block:'B',number:'74',beds:'3',area:118.4},
-                    {plotNumber:'B 4-1',level:'4',block:'B',number:'75',beds:'2',area:76.5},
-                    {plotNumber:'B 4-2',level:'4',block:'B',number:'76',beds:'1',area:51.3},
-                    {plotNumber:'B 4-3',level:'4',block:'B',number:'77',beds:'2',area:80.6},
-                    {plotNumber:'B 4-4',level:'4',block:'B',number:'78',beds:'1',area:53.5},
-                    {plotNumber:'B 4-5',level:'4',block:'B',number:'79',beds:'2',area:76.5},
-                    {plotNumber:'B 4-6',level:'4',block:'B',number:'80',beds:'3',area:111.5},
-                    {plotNumber:'B 4-7',level:'4',block:'B',number:'81',beds:'3',area:118.4},
-                    {plotNumber:'C 0-1',level:'0',block:'C',number:'82',beds:'2',area:85.1},
-                    {plotNumber:'C 0-2',level:'0',block:'C',number:'83',beds:'1',area:53.5},
-                    {plotNumber:'C 0-3',level:'0',block:'C',number:'84',beds:'2',area:65.5},
-                    {plotNumber:'C 0-4',level:'0',block:'C',number:'85',beds:'2',area:76},
-                    {plotNumber:'C 0-5',level:'0',block:'C',number:'86',beds:'2',area:76.4},
-                    {plotNumber:'C 0-6',level:'0',block:'C',number:'87',beds:'1',area:51.3},
-                    {plotNumber:'C 0-7',level:'0',block:'C',number:'88',beds:'2',area:65},
-                    {plotNumber:'C 0-8',level:'0',block:'C',number:'89',beds:'2',area:69.9},
-                    {plotNumber:'C 1-1',level:'1',block:'C',number:'90',beds:'1',area:55},
-                    {plotNumber:'C 1-2',level:'1',block:'C',number:'91',beds:'1',area:53.5},
-                    {plotNumber:'C 1-3',level:'1',block:'C',number:'92',beds:'2',area:65.5},
-                    {plotNumber:'C 1-4',level:'1',block:'C',number:'93',beds:'2',area:76},
-                    {plotNumber:'C 1-5',level:'1',block:'C',number:'94',beds:'2',area:76.4},
-                    {plotNumber:'C 1-6',level:'1',block:'C',number:'95',beds:'1',area:51.3},
-                    {plotNumber:'C 1-7',level:'1',block:'C',number:'96',beds:'2',area:65},
-                    {plotNumber:'C 1-8',level:'1',block:'C',number:'97',beds:'2',area:78.7},
-                    {plotNumber:'C 1-9',level:'1',block:'C',number:'98',beds:'2',area:66.3},
-                    {plotNumber:'C 2-1',level:'2',block:'C',number:'99',beds:'1',area:55},
-                    {plotNumber:'C 2-2',level:'2',block:'C',number:'100',beds:'1',area:53.5},
-                    {plotNumber:'C 2-3',level:'2',block:'C',number:'101',beds:'2',area:65.5},
-                    {plotNumber:'C 2-4',level:'2',block:'C',number:'102',beds:'2',area:76},
-                    {plotNumber:'C 2-5',level:'2',block:'C',number:'103',beds:'2',area:76.4},
-                    {plotNumber:'C 2-6',level:'2',block:'C',number:'104',beds:'1',area:51.3},
-                    {plotNumber:'C 2-7',level:'2',block:'C',number:'105',beds:'2',area:65},
-                    {plotNumber:'C 2-8',level:'2',block:'C',number:'106',beds:'2',area:78.7},
-                    {plotNumber:'C 2-9',level:'2',block:'C',number:'107',beds:'2',area:66.3},
-                    {plotNumber:'C 3-1',level:'3',block:'C',number:'108',beds:'2',area:80},
-                    {plotNumber:'C 3-2',level:'3',block:'C',number:'109',beds:'1',area:53.5},
-                    {plotNumber:'C 3-3',level:'3',block:'C',number:'110',beds:'2',area:65.5},
-                    {plotNumber:'C 3-4',level:'3',block:'C',number:'111',beds:'2',area:80.6},
-                    {plotNumber:'C 3-5',level:'3',block:'C',number:'112',beds:'1',area:51.3},
-                    {plotNumber:'C 3-6',level:'3',block:'C',number:'113',beds:'2',area:65},
-                    {plotNumber:'C 3-7',level:'3',block:'C',number:'114',beds:'3',area:78.7},
-                    {plotNumber:'C 4-1',level:'4',block:'C',number:'115',beds:'2',area:80},
-                    {plotNumber:'C 4-2',level:'4',block:'C',number:'116',beds:'1',area:53.5},
-                    {plotNumber:'C 4-3',level:'4',block:'C',number:'117',beds:'2',area:65.5},
-                    {plotNumber:'C 4-4',level:'4',block:'C',number:'118',beds:'2',area:80.6},
-                    {plotNumber:'C 4-5',level:'4',block:'C',number:'119',beds:'1',area:51.3},
-                    {plotNumber:'C 4-6',level:'4',block:'C',number:'120',beds:'2',area:65},
-                    {plotNumber:'C 4-7',level:'4',block:'C',number:'121',beds:'3',area:78.7}
-                  ]
+                  ],
+      bed1:false,
+      bed2:false,
+      bed3:false,
+      floor0:false,    
+      floor1:false,             
+      floor2:false,             
+      floor3:false,             
+      floor4:false,             
     }
     this.handleCheck = this.handleCheck.bind(this);
+    this.filterApartments = this.filterApartments.bind(this);
   }
+
+  handleCheck = () => {
+    this.setState({
+      bed1: !this.state.bed1
+    });
+    this.filterApartments();
+  }
+
+  filterApartments() {
+    if(!this.state.bed1) {
+      this.setState({
+        filteredApartments: this.state.apartments.filter((apartment) => {
+          return apartment.beds === "1"
+        })
+      })
+    } else {
+      this.setState({
+        filteredApartments: []
+      })
+    }
+  }
+ 
+
     render() {
+
+      let msg=" "
+      if(this.state.bed1){
+          msg="checked!"
+      }else{
+          msg="not checked!"
+      }
+      
       return(
         <Container fluid>
           <Row>
@@ -206,11 +163,56 @@ class App extends React.Component {
           </Row>
           <Row >
             <Col className='filter-background' md={2}>
-              <FilterComponent />
+              <Container className='container-border'>
+                <br/>
+                <Container >
+                  <Row>
+                      <h4>Filters</h4>
+                  </Row>
+                </Container>
+                  <hr className='line'/>
+                <Container>
+                  <Row>
+                    <h5>Bedrooms</h5>
+                  </Row>               
+                 
+                  <div>
+                    <input  type="checkbox" 
+                            onChange={this.handleCheck}
+                            defaultChecked={this.state.bed1}
+                            
+                      />
+                    <label>1 bed</label>
+                    <p>this box is {msg}</p>
+                  </div>
+                    {/* {makeCheckBox('1 Bed', '1-bed', 1)}
+                    {makeCheckBox('2 Bed', '2-bed', 1)}
+                    {makeCheckBox('3 Bed', '3-bed', 1)}      */}
+                    <br/>
+                </Container>
+                <Container>
+                  <Row>
+                    <h5>Floor</h5>
+                  </Row>
+                    {/* {makeCheckBox('Ground', '0', 2)}
+                    {makeCheckBox('First', '1', 2)}
+                    {makeCheckBox('Second', '2', 2)}
+                    {makeCheckBox('Third', '3', 2)}
+                    {makeCheckBox('Fourth', '4', 2)} */}
+                </Container>
+                <hr className='line'/>
+                <Container >
+                <Row>
+                    {/* {makeCheckBox('Show All', 'all', 3)} */}
+                  </Row>
+                </Container>
+              </Container>
+
+              {/* <FilterComponent /> */}
             </Col>            
             <Col className='light-background' md={10}>
               <ContainerInterface 
-                apartments={this.state.apartments}
+                apartments={this.state.filteredApartments}
               />
             </Col>
           </ Row>
@@ -260,42 +262,23 @@ class ContainerInterface extends React.Component {
 
 
 
-const handleCheck = () => {
-  setIsChecked(!isChecked);
-}
-
 class FilterComponent extends React.Component {
-  render() {
 
-    // const makeCheckBox = function(label, id, group) {
-    //   return <Row>
-    //           <Col >
-    //             <Form>
-    //               <Form.Check
-    //                 inline
-    //                 label={label}
-    //                 name={group}
-    //                 type='checkbox'
-    //                 id={`${id}-checkbox`}
-    //               />
-    //             </Form>
-    //           </Col>
-    //         </Row>
-    // }
-    const [isChecked, setIsChecked] = useState(false);
     
-    const makeCheckBox = function(label, id, group) {
+    render() {
 
+    
+
+    const makeCheckBox = function(label, id, group) {
       return <Row>
               <Col >
                 <Form>
-                  <input 
-                    type="checkbox"
-                    id={id}
-                    name={id}
-                    value={label}
-                    checked={isChecked}
-                    onChange={handleCheck}
+                  <Form.Check
+                    inline
+                    label={label}
+                    name={group}
+                    type='checkbox'
+                    id={`${id}-checkbox`}
                   />
                 </Form>
               </Col>
@@ -303,8 +286,8 @@ class FilterComponent extends React.Component {
     }
 
 
-    return (    
 
+    return (    
     <Container className='container-border'>
       <br/>
       <Container >
@@ -317,6 +300,7 @@ class FilterComponent extends React.Component {
         <Row>
           <h5>Bedrooms</h5>
         </Row>
+
           {makeCheckBox('1 Bed', '1-bed', 1)}
           {makeCheckBox('2 Bed', '2-bed', 1)}
           {makeCheckBox('3 Bed', '3-bed', 1)}     
